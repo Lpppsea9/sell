b443acf9617fdc4fce009eb52f7d960fa114c922
Lpppsea9 <1638697794@qq.com>
2020-09-23 23:03:39 +0800

    9/23

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    host: 'localhost'
+  } // 解决了vue cli3 wds disconnected控制台报错的方法
+}
